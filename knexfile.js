5651f87d05a75d13fa7a057fa5c715c3171f9da7
RKcod <116556773+RKcod@users.noreply.github.com>
2025-01-22 13:32:48 +0100

    fix

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,13 @@
 const path = require('path');
-
+require('dotenv').config();
 module.exports = {
     development: {
         client: 'pg',
         connection: {
-            host: 'db',
+            host: process.env.DB_HOST,
             user: process.env.DB_USER ,
             password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            port: process.env.DB_PORT,
+            database: process.env.DB_NAME
         },
         migrations: {
             directory: path.resolve(__dirname, 'src/databases/migrations'),
