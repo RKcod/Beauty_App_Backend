235185928aca258a403ff9cf57e16cae31bbc091
RKcod <116556773+RKcod@users.noreply.github.com>
2025-01-23 13:31:40 +0100

    adding resources

--- src/modules/user/interfaces/controllers/GetAllUsersController.js
+++ src/modules/user/interfaces/controllers/GetAllUsersController.js
@@ -1,14 +1,15 @@
 
 const getAllUsersUseCase = require("../../core/usecases/GetAllUserUseCase");
+const getUsersResource = require('../resources/GetAllUsersResource');
 
 class GetAllUsersController {
   static async getAll(req, res) {
     try {
 
       const users = await getAllUsersUseCase.getAll();
-
+      const usersFormatted = getUsersResource.collection(users);
       return res.status(201).json({
-       users
+        'data' :  usersFormatted
       });
     } catch (error) {
       return res.status(400).json({ error: error.message });
